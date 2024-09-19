309b2aaadcf0308e9b5d08ea8880c8c56390b9da
Josh K Laptop <joshuafkaplan@gmail.com>
2024-09-18 21:25:16 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      mono: ['ui-monospace', 'SFMono-regular'],
+    },
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-12deg)" },
+          "50%": { transform: "rotate(12deg)" },
+        },
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
+    },
+  },
+  plugins: [],
+}
+
